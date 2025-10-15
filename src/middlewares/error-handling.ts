18ac650a903c8f1cb169ec202dd927fbbd7c8fbf
artmorai77 <arturmorais.ald@gmail.com>
2025-10-15 14:31:30 -0300

    Adicionando Zod

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,15 +1,22 @@
 import { AppError } from "@/utils/AppError";
 import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
 
 export function errorHandling(
   error: any,
-  req: Request, 
-  res: Response, 
+  req: Request,
+  res: Response,
   next: NextFunction
 ) {
   if (error instanceof AppError) {
-    res.status(error.statusCode).json({ message: error.message})
+    res.status(error.statusCode).json({ message: error.message });
+  }
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      message: "validation error",
+      issues: error.format(),
+    });
   }
 
-  return res.status(500).json({ message: error.message})
+  return res.status(500).json({ message: error.message });
 }
