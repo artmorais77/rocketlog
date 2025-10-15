1f31ccde52fb2b48f3f80e4b28ccb2ba239f5345
artmorai77 <arturmorais.ald@gmail.com>
2025-10-15 16:28:12 -0300

    Validando Dados do Usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class UsersController {
   index(req: Request, res: Response) {
@@ -6,7 +7,15 @@ class UsersController {
   }
 
   create(req: Request, res: Response) {
-    res.status(201).json({ message: "ok" });
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(req.body);
+
+    res.status(201).json({ name, email, password });
   }
 }
 
