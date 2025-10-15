a72546a182496e8fcda77d1b3dd405fae139b952
artmorai77 <arturmorais.ald@gmail.com>
2025-10-15 17:48:03 -0300

    Token do Usuário

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,35 +1,43 @@
+import { authConfig } from "@/configs/auth";
 import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/AppError";
 import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import z from "zod";
 
 class SessionsController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-
       const bodySchema = z.object({
         email: z.string().trim().email(),
         password: z.string().trim().min(6),
       });
-      
+
       const { email, password } = bodySchema.parse(req.body);
-      
+
       const user = await prisma.user.findFirst({ where: { email } });
-      
+
       if (!user) {
         throw new AppError("Invalid email or password", 401);
       }
-      
+
       const passwordMatched = await compare(password, user.password);
 
       if (!passwordMatched) {
         throw new AppError("Invalid email or password", 401);
       }
-      
-      res.status(201).json({ message: "ok" });
-    } catch(error) {
-      next(error)
+
+      const { secret, expiresIn } = authConfig.jwt;
+
+      const token = sign({ role: user.role ?? "customer" }, secret, {
+        subject: user.id,
+        expiresIn,
+      });
+
+      res.status(201).json(token);
+    } catch (error) {
+      next(error);
     }
   }
 }
