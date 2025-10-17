41f5bf96ae5622e394cc5f8445962a55fee67cd8
artmorai77 <arturmorais.ald@gmail.com>
2025-10-17 10:34:42 -0300

    Removendo Usuário de Teste

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,3 +1,24 @@
 import { app } from "@/app";
+import { prisma } from "@/database/prisma";
 import request from "supertest"
 
+describe("UserController", () => {
+let user_id: string
+
+  afterAll(async () => {
+    await prisma.user.delete({where: {id: user_id}})
+  })
+
+  it("should create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "test user",
+      email: "testuser@email.com",
+      password: "password123"
+    })
+
+    expect(response.status).toBe(201),
+    expect(response.body).toHaveProperty("id")
+    expect(response.body.name).toBe("test user")
+    user_id = response.body.id
+  })
+})
